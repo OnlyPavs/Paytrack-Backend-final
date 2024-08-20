2dc23014dab915a7601b0c7c57e4ceb93dce5bcc
OnlyPavs <116968152+OnlyPavs@users.noreply.github.com>
2024-08-20 23:22:04 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+dbPassword =
+  "mongodb+srv://spkpower26:Spkpower%4026@cluster0.tkwlljr.mongodb.net/?retryWrites=true&w=majority"
+
+secretOrKey = 'APP_SECRET_KEY';
+
+module.exports = {
+  mongoURI: dbPassword,
+  secretOrKey,
+};
